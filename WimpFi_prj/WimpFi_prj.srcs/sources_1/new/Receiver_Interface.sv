f86064197ca1e0bb3dec146dbde1f6e74f1c310c
flynng <flynng>
2016-11-17 13:09:57 -0500

    Fixed module naming order

--- WimpFi_prj/WimpFi_prj.srcs/sources_1/new/Receiver_Interface.sv
+++ WimpFi_prj/WimpFi_prj.srcs/sources_1/new/Receiver_Interface.sv
@@ -35,13 +35,13 @@ module Receiver_Interface(
 
 	// Create the modules to attach together
 
-	U_RX_MX mx_rcvr (.clk, .reset, .rxd, .cardet, .data(data_rx), .write(write_rx), .error(),
+	mx_rcvr U_RX_MX (.clk, .reset, .rxd, .cardet, .data(data_rx), .write(write_rx), .error(),
 					.error1(), .error2(), .error3(), .looking(), .error_count());
 
-	U_FSM_ADDR FSM_Address_check (.clk, .reset, .write(write_rx), .cardet, .address(mac), .data(data_rx),
+	FSM_Address_check U_FSM_ADDR (.clk, .reset, .write(write_rx), .cardet, .address(mac), .data(data_rx),
 									.write_enb, .clear_fifo);
 	
 	// Active low reset
-	U_FIFO p_fifo (.clk, .rst(~reset), .clr(clear_fifo), .din(data_rx), .we, .re(rrd), .full(), .empty, .dout(rdata));
+	p_fifo U_FIFO (.clk, .rst(~reset), .clr(clear_fifo), .din(data_rx), .we, .re(rrd), .full(), .empty, .dout(rdata));
 
 endmodule

eb1259ca67b39bdc1e2de18ab8f8153c2ec55306
flynng <flynng>
2016-11-19 09:53:05 -0500

    Fixed inferred latch on use_fsm in tx side

--- WimpFi_prj/WimpFi_prj.srcs/sources_1/new/FSM_fifo_to_send.sv
+++ WimpFi_prj/WimpFi_prj.srcs/sources_1/new/FSM_fifo_to_send.sv
@@ -67,62 +67,63 @@ module FSM_fifo_to_send(
 		xrdy = 0;
 		data = 0;
 		next = IDLE;
+		use_fsm = 0;
 		case(state)
-		IDLE:
-			begin
-				if(empty) next = IDLE; // don't send if there is nothing to send
-				else next = xsnd ? TIMING_CHECK : IDLE;
-				xrdy = 1;
-			end
-		TIMING_CHECK:
-			begin
-				rts = 1;
-				// Verify cts signal
-				next = cts ? LOAD_PREAMBLE1 : TIMING_CHECK;
-			end
-		LOAD_PREAMBLE1:
-			// Wait till we are ready
-			next = rdy ? SEND_PREAMBLE1 : LOAD_PREAMBLE1;
-		SEND_PREAMBLE1:
-			begin
-				send = 1;
-				data = PREAMBLE;
-				use_fsm = 1;
-				// Wait until ready falls before advancing
-				next = rdy ? SEND_PREAMBLE1 : LOAD_PREAMBLE2;
-			end
-		LOAD_PREAMBLE2:
-			next = rdy ? SEND_PREAMBLE2 : LOAD_PREAMBLE2;
-		SEND_PREAMBLE2:
-			begin
-				send = 1;
-				data = PREAMBLE;
-				use_fsm = 1;
-				next = rdy ? SEND_PREAMBLE2 : LOAD_SFD;
-			end
-		LOAD_SFD:
-			next = rdy ? SEND_SFD : LOAD_SFD;
-		SEND_SFD:
-			begin
-				data = SFD;
-				use_fsm = 1;
-				send = 1;
-				next = rdy ? SEND_SFD : STAND_BY;
-			end
-		STAND_BY:
-			next = rdy ? SEND : STAND_BY;
-		SEND:
-			begin
-				send = 1;
-				next = rdy ? SEND : GET_NEXT_BYTE;
-			end		
-		GET_NEXT_BYTE:
-			begin
-				read_data = 1;
-				next = CHECK_EMPTY; 
-			end
-		CHECK_EMPTY:
-			next = empty ? IDLE : STAND_BY;
+			IDLE:
+				begin
+					if(empty) next = IDLE; // don't send if there is nothing to send
+					else next = xsnd ? TIMING_CHECK : IDLE;
+					xrdy = 1;
+				end
+			TIMING_CHECK:
+				begin
+					rts = 1;
+					// Verify cts signal
+					next = cts ? LOAD_PREAMBLE1 : TIMING_CHECK;
+				end
+			LOAD_PREAMBLE1:
+				// Wait till we are ready
+				next = rdy ? SEND_PREAMBLE1 : LOAD_PREAMBLE1;
+			SEND_PREAMBLE1:
+				begin
+					send = 1;
+					data = PREAMBLE;
+					use_fsm = 1;
+					// Wait until ready falls before advancing
+					next = rdy ? SEND_PREAMBLE1 : LOAD_PREAMBLE2;
+				end
+			LOAD_PREAMBLE2:
+				next = rdy ? SEND_PREAMBLE2 : LOAD_PREAMBLE2;
+			SEND_PREAMBLE2:
+				begin
+					send = 1;
+					data = PREAMBLE;
+					use_fsm = 1;
+					next = rdy ? SEND_PREAMBLE2 : LOAD_SFD;
+				end
+			LOAD_SFD:
+				next = rdy ? SEND_SFD : LOAD_SFD;
+			SEND_SFD:
+				begin
+					data = SFD;
+					use_fsm = 1;
+					send = 1;
+					next = rdy ? SEND_SFD : STAND_BY;
+				end
+			STAND_BY:
+				next = rdy ? SEND : STAND_BY;
+			SEND:
+				begin
+					send = 1;
+					next = rdy ? SEND : GET_NEXT_BYTE;
+				end		
+			GET_NEXT_BYTE:
+				begin
+					read_data = 1;
+					next = CHECK_EMPTY; 
+				end
+			CHECK_EMPTY:
+				next = empty ? IDLE : STAND_BY;
 		endcase
 			
 	end

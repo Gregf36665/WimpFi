6933c2fbf177187db789b5ea2446768998c11ccc
Gregf36665 <Flynng@lafayette.edu>
2016-12-02 09:38:22 -0500

    Added updated statistics

--- LFSR_stats.r
+++ LFSR_stats.r
@@ -1,16 +1,22 @@
 nSims <- 5000 #number of simulated experiments
-p <-numeric(nSims) #set up empty container for all simulated p-values
+max_val <-numeric(nSims) #set up empty container for all simulated max values
+min_val <-numeric(nSims) #set up empty container for all simulated min values
+mean_val <-numeric(nSims) #set up empty container for all simulated mean values
 values = read.table("data.txt")
 
 for(i in 1:nSims){ #for each simulated experiment
   x<-runif(n = 2000, min = 1, max = 63) 
   y<-values[[i]]
   
-  
- z<-t.test(x,y) #perform the t-test
- p[i]<-z$p.value #get the p-value and store it
+ max_val[i]<-max(y)
+ min_val[i]<-min(y)
+ mean_val[i]<-mean(y)
 }
  
 #now plot the histogram
-#plot(x, y)
 hist(p, main="Histogram of p-values compared to uniform distribution", xlab=("Observed p-value"))
+hist(max_val, main="Histogram of max values from LFSR", xlab=("Max value"))
+hist(min_val, main="Histogram of min values from LFSR", xlab=("Min value"))
+hist(mean_val, main="Histogram of mean values from LFSR", xlab=("Mean value"))
+
+t.test(mean_val);
